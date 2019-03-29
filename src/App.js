579c55668b48107ff5e81d5e03c1819af7d53db4
Paulo Angeles <paulo.angeles@redudg.udg.mx>
2019-03-29 11:23:41 -0600

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import AgregarCita from './components/AgregarCita'
 
 class App extends Component {
+  
+  agregarCita = () => {
+    console.log("Desde app.js");
+  }
+
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Header 
+          title={'Administrador de Paciente de Veterinaria'}
+          />
+          <div className="row">
+            <div className="col-md-6">
+              <AgregarCita
+                agregarCita={this.agregarCita}
+              />
+            </div>
+          </div>
+
       </div>
     );
   }
